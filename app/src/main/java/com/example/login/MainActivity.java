a26dcf84fa660ec539439934ba50c6eaa91f8a5a
DennisCornel <denniscornelsosa@gmail.com>
2022-04-24 23:21:49 -0600

    Commit Conexion Firebase

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     //metodos para hacer los saltos de pantalla necesarios
@@ -19,5 +28,7 @@ public class MainActivity extends AppCompatActivity {
     public void Inicio(View view){
         Intent inicio = new Intent(this, InicioActivity.class);
         startActivity(inicio);
+
+
     }
 }
