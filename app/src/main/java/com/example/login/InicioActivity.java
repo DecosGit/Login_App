602c9a4b3b5369c09bf954975fc594b72f968b3d
DennisCornel <denniscornelsosa@gmail.com>
2022-04-29 19:26:48 -0600

    Usuario Autenticado Conexion a Firebase Exitosa

--- app/src/main/java/com/example/login/InicioActivity.java
+++ app/src/main/java/com/example/login/InicioActivity.java
@@ -5,15 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class InicioActivity extends AppCompatActivity {
+
+    private Button btSalir;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
+        btSalir = findViewById(R.id.btSal);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btSalir.setOnClickListener(view -> {
+            mAuth.signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+        });
     }
+
 
 
     public void opPiloto(View view) {
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -6,29 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
-    }
-
-    //metodos para hacer los saltos de pantalla necesarios
-
-    public void Inicio(View view){
-        Intent inicio = new Intent(this, InicioActivity.class);
-        startActivity(inicio);
-
+        mAuth = FirebaseAuth.getInstance();
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if(user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }else{
+            startActivity(new Intent(MainActivity.this, InicioActivity.class));
+        }
+    }
+
+
 }
